8b6ba5695432f92658ebbe60de0abf3682e08927
IVO.NASCIMENTO <ivo.oliveira.n@gmail.com>
2025-08-08 21:05:20 -0300

    Create aula1.java

---
+++ aula1.java
@@ -0,0 +1,25 @@
+package modulo1;
+
+public class Cupomfiscal {
+
+	public static void main(String[] args) {
+	//Cabeçalho da loja	
+		System.out.println("============================SUPERMERCADO BOM PREÇO================================");
+		System.out.println("ENDEREÇO: Rua da Flores, 123 - Centro, Fortaleza CE");
+		System.out.println("CNPJ: 12.345.6789/0001-90");
+		System.out.println("__________________________________________________________________________________");
+	//Lista de Produtos
+		System.out.println("Arroz Camil 5kg...........................................................R$ 25,90");
+		System.out.println("Feijão Kicaldo 1kg.........................................................R$ 8,00");
+		System.out.println("Açucar Cristal 2kg.........................................................R$ 7,90");
+		System.out.println("----------------------------------------------------------------------------------");
+	//Total da Compra
+		System.out.println("Total.....................................................................R$ 41,10");
+	//Mensagem Final
+		System.out.println("----------------------------------------------------------------------------------");
+		System.out.println("Obrigado pela preferência.");
+		System.out.println("Volte sempre!");
+
+	}
+
+}
